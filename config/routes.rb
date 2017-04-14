87635e25c67c8505dba4f1a952763845ca3d27a7
wangxing <18912540921@163.com>
2017-04-14 22:05:43 +0800

    model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+    end
+
+  end
 
   namespace :admin do
     resources :jobs do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411133125) do
+ActiveRecord::Schema.define(version: 20170414133612) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
