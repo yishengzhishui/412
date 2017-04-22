9deb40b3c809cd9deba65916c6b3ef534f8371b5
wangxing <18912540921@163.com>
2017-04-22 09:21:58 +0800

    create

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -19,6 +19,7 @@ class Admin::JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
+    @job.user = current_user
 
     if @job.save
       redirect_to admin_jobs_path
