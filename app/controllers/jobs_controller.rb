d6890b337af68535fc9a774371577bc11208041a
wangxing <18912540921@163.com>
2017-04-30 10:38:03 +0800

    add on jobs_controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -33,6 +33,28 @@ class JobsController < ApplicationController
     end
   end
 
+  def operation
+    @jobs = case params[:order]
+    when 'by_lower_bound'
+      Job.published.where(:category => "海洋开发及航运").order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 10)
+    when 'by_upper_bound'
+      Job.published.where(:category => "海洋开发及航运").order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 10)
+    else
+      Job.published.where(:category => "海洋开发及航运").recent.paginate(:page => params[:page], :per_page => 10)
+    end
+  end
+
+  def produce
+    @jobs = case params[:order]
+    when 'by_lower_bound'
+      Job.published.where(:category => "设计与制造").order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 10)
+    when 'by_upper_bound'
+      Job.published.where(:category => "设计与制造").order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 10)
+    else
+      Job.published.where(:category => "设计与制造").recent.paginate(:page => params[:page], :per_page => 10)
+    end
+  end
+
   def join
     @job = Job.find(params[:id])
 
