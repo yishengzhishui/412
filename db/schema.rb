90d5479c0f4a8d55030c1f00e2479ed4869ffdc6
wangxing <18912540921@163.com>
2017-05-01 21:48:25 +0800

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429145115) do
+ActiveRecord::Schema.define(version: 20170501133839) do
 
   create_table "job_relationships", force: :cascade do |t|
     t.integer  "job_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170429145115) do
     t.integer  "user_id"
     t.string   "category"
     t.string   "city"
+    t.string   "company"
   end
 
   create_table "resumes", force: :cascade do |t|
