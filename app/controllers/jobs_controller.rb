ac28f94f9cde8aeb0e1bbbbf2c07de77cf61ccb2
wangxing <18912540921@163.com>
2017-05-01 21:44:33 +0800

    add company

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -104,7 +104,7 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title,:description,:wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden, :category, :city)
+    params.require(:job).permit(:title,:description,:wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden, :category, :city, :company)
   end
 
 
