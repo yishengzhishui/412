74fc33dba6e97228f08433709d02afec6f5b9c59
wangxing <18912540921@163.com>
2017-05-13 16:07:16 +0800

    user image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
-  
+  def avatar_url(user, size)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=retro"
+  end
+
+  def render_user_avatar(user, size)
+    if user.image.present?
+      user.image
+    else
+      avatar_url(user, size)
+    end
+  end
 end
